539bc3075a3da5132e93ac75f6cf18fda98c460a
l33th <l33th@protonmail.ch>
2021-09-30 19:40:18 +0200

    Tours Component with Domain Data

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
 import React, { useState, useEffect } from "react";
 import Loading from "./components/Loading";
 import Tours from "./components/Tours";
-// ATTENTION!!!!!!!!!!
-// I SWITCHED TO PERMANENT DOMAIN
+
+const url = "https://course-api.com/react-tours-project";
+
 function App() {
 	const [loading, setLoading] = useState(true);
 	const [tours, setTours] = useState([]);
+
+	const fetchTours = async () => {
+		setLoading(true);
+		try {
+			const response = await fetch(url);
+			const tours = await response.json();
+			setLoading(false);
+			setTours(tours);
+		} catch (error) {
+			setLoading(false);
+			console.log(error);
+		}
+	};
+	useEffect(() => {
+		fetchTours();
+	}, []);
+
 	if (loading) {
 		return (
 			<main>
@@ -15,10 +33,10 @@ function App() {
 	}
 
 	return (
-    <main>
-      <Tours />
-    </main>
-  )
+		<main>
+			<Tours />
+		</main>
+	);
 }
 
 export default App;
