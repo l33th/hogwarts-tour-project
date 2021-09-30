33353ece7715ed23f4de9add40f02917c4761e25
l33th <l33th@protonmail.ch>
2021-09-30 18:15:09 +0200

    Hogwarts Tour Project Setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React, { useState, useEffect } from 'react'
+import Loading from './components/Loading'
+import Tours from './components/Tours'
+// ATTENTION!!!!!!!!!!
+// I SWITCHED TO PERMANENT DOMAIN
+function App() {
+  return <h2>Hogwarts Tour Project Setup</h2>
+}
+
+export default App
