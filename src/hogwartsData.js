a0aa25579db1cd3dc3edb1b90c69cab7503c6fb8
l33th <l33th@protonmail.ch>
2021-09-30 19:51:18 +0200

    Tours Display

--- src/hogwartsData.js
+++
@@ -1,9 +0,0 @@
-const hogwartsData =  {
-        id: 1,
-        name: "Hogwarts Castle",
-        info: "The castle was set upon huge rocks above the Black Lake. Its three highest towers were the Astronomy, Ravenclaw, and Gryffindor Towers. The castle's 142 staircases were known to change position. The castle was known for its many updates and changes in layout throughout the years.It should be noted that some rooms in the school tended to move around, as did the steps on the Grand Staircase. Albus Dumbledore once noted that even he did not know all of Hogwarts' secrets. The castle had been around for centuries, and consequently had a long history of ancient magic.",
-        image: "./src/img/Hogwarts.jpg",
-        price: "$6200"
-}
-
-export default hogwartsData;
